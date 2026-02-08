a7cc27b878762e77f11dce2f68708d6c3e2d03c3
BappiGorain <bappigorain08312@gmail.com>
2026-02-08 19:01:19 +0530

    products.html added

--- src/main/java/com/ecommerce/controller/TestController.java
+++ src/main/java/com/ecommerce/controller/TestController.java
@@ -1,15 +1,63 @@
 package com.ecommerce.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ecommerce.model.Product;
+import com.ecommerce.service.ProductService;
+
+
+@Controller
 public class TestController
 {
 
-    @GetMapping("/test")
-    public String testController()
+    final private ProductService productService;
+
+    TestController(ProductService productService)
     {
-        return "Project is working fine"; 
+        this.productService = productService;
     }
+
+
+    @GetMapping("/product")
+    public String allProducts(Model model)
+    {
+        System.out.println("All product");
+
+        List<Product> allProduct = productService.getAllProduct();
+
+        model.addAttribute("products",allProduct);
+
+        return "products";
+    }
+
+
+    @GetMapping("/test")
+    public String test(Model model)
+    {
+        
+        model.addAttribute("name","Bappi");
+
+
+
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("Tiger");
+        animals.add("Elephant");
+        animals.add("Cat");
+        animals.add("Dog");
+        animals.add("cheath");
+
+        model.addAttribute("animalList",animals);
+        
+        
+        
+        
+        
+        return "test";
+    }
+    
 }
--- src/main/java/com/ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/exception/GlobalExceptionHandler.java
@@ -3,16 +3,14 @@ package com.ecommerce.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import com.ecommerce.helper.ApiResponse;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler
 {
-
-
+    
     // ResourceNotFoundException Handler
 
     @ExceptionHandler(ResourceNotFoundException.class)
