21dfd63d557270aaf5f9b7ba967e2548888d5907
BappiGorain <bappigorain08312@gmail.com>
2026-03-09 09:54:24 +0530

    some bugs are fixed

--- src/main/java/com/ecommerce/controller/AdminController.java
+++ src/main/java/com/ecommerce/controller/AdminController.java
@@ -1,15 +1,42 @@
 package com.ecommerce.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ecommerce.service.CategoryService;
+import com.ecommerce.service.ProductService;
+import com.ecommerce.service.UserService;
 
 @Controller
 public class AdminController 
 {
 
-   @GetMapping("/admin/dashboard")
-    public String adminDashboard()
+    final private UserService userService;
+    final private CategoryService categoryService;
+    final private ProductService productService;
+
+    AdminController(UserService userService, CategoryService categoryService, ProductService productService
+    )
     {
+        this.userService = userService;
+        this.categoryService = categoryService;
+        this.productService = productService;
+    }
+
+
+   @GetMapping("/admin/dashboard")
+    public String adminDashboard(Model model)
+    {
+
+        int userSize = userService.getAllUser().size();
+        int categorySize = categoryService.getAllCategories().size();
+        int productSize = productService.getAllProduct().size();
+
+        model.addAttribute("totalUsers",userSize);
+        model.addAttribute("totalCategories",categorySize);
+        model.addAttribute("totalProducts",productSize);
+
         
 
         return "admin/dashboard";
